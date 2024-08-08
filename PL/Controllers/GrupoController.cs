e4cd59ff764cfc07b0cdfbd4f627c73bbb6b8f9f
Perla53 <sanchezmontejanop@gmail.com>
2024-08-08 12:43:08 -0600

    PL Grupo

--- PL/Controllers/GrupoController.cs
+++ PL/Controllers/GrupoController.cs
@@ -8,17 +8,27 @@ namespace PL.Controllers
 {
     public class GrupoController : Controller
     {
+        BL.Grupo grupoBL = new BL.Grupo();
         // GET: Grupo
         [HttpGet]
         public ActionResult GetAll()
         {
-            BL.Grupo grupoBL = new BL.Grupo();
             ML.Result result = grupoBL.GetAll();
             ML.Grupo grupo = new ML.Grupo();
 
             grupo.Grupos = result.Objects;
 
             return View(grupo);
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int idGrupo)
+        {
+            ML.Grupo grupo = new ML.Grupo();
+            grupo.IdGrupo = idGrupo;
+            ML.Result result = grupoBL.Delete(grupo);
+
+            return View(result);
         }
     }
 }
