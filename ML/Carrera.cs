9ad361118998d20e16f850725ec84ef7984b92ec
Perla53 <sanchezmontejanop@gmail.com>
2024-08-08 10:22:54 -0600

    ML Carrera

---
+++ ML/Carrera.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ML
+{
+    public class Carrera
+    {
+        public int IdCarrera { get; set; }
+        public string Nombre { get; set; }
+    }
+}
