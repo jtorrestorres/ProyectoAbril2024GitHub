12277a5309a926a5c6ab916f37c82c34b23bd627
Perla53 <sanchezmontejanop@gmail.com>
2024-08-08 12:52:34 -0600

    PL Grupo

--- PL/Controllers/GrupoController.cs
+++ PL/Controllers/GrupoController.cs
@@ -34,7 +34,7 @@ namespace PL.Controllers
             }
             else
             {
-                ViewBag.Mensaje = "No se ha eliminado el registro" + result.ErrorMenssage;
+                ViewBag.Mensaje = "No se ha eliminado el registro" + result.ErrorMessage;
             }
 
             return PartialView("Modal");
