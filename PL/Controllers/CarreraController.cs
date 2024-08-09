1644f8b0b38a468b9534968ce995956acf7ae6b5
Perla53 <sanchezmontejanop@gmail.com>
2024-08-09 11:25:50 -0600

    Form CarreraAdd

--- PL/Controllers/CarreraController.cs
+++ PL/Controllers/CarreraController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Timers;
 using System.Web;
 using System.Web.Mvc;
 
@@ -45,6 +46,30 @@ namespace PL.Controllers
             }
 
             return View(carrera);
+        }
+
+        [HttpPost]
+        public ActionResult Form(ML.Carrera carrera)
+        {
+            ML.Result result=new ML.Result();
+            if (carrera.IdCarrera>0)
+            {
+
+            }
+            else
+            {
+                result = carreraBL.Add(carrera);
+            }
+
+            if (result.Correct)
+            {
+                ViewBag.Mensaje = "Se agrego correctamente.";
+            }
+            else
+            {
+                ViewBag.Mensaje = "No se agrego correctamente " + result.ErrorMessage;
+            }
+            return PartialView("Modal");
         }
 
         [HttpGet]
