5301b1f1fbe5ba3c3955e381bcfd41ccd932206c
Perla53 <sanchezmontejanop@gmail.com>
2024-08-08 18:45:47 -0600

    CarreraGetAll

---
+++ PL/Controllers/CarreraController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PL.Controllers
+{
+    public class CarreraController : Controller
+    {
+        // GET: Carrera
+        [HttpGet]
+        public ActionResult GetAll()
+        {
+            return View();
+        }
+    }
+}
